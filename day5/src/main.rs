8b4d53028ae40ec03ce42caff4afe2326af5125c
Ruben Dura Tari <ruben.duratari@skyscanner.net>
2017-12-08 10:42:59 +0000

    Solve day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -5,21 +5,33 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     let maze: Vec<i32> = input.trim().lines().map(|l| l.parse::<i32>().expect("Not a number")).collect();
-    let result = maze_exit_steps(&maze);
+    let result = maze_exit_steps(&mut maze.clone(), incrementer2);
     println!("{}", result);
 }
 
-fn maze_exit_steps(maze: &[i32]) -> i32 {
-    let current = 0;
-    let count = 0;
+
+fn incrementer(input: i32) -> i32 { input + 1 }
+fn incrementer2(input: i32) -> i32 {
+    if input >= 3 {
+        input - 1
+    }
+    else {
+        incrementer(input)
+    }
+}
+
+fn maze_exit_steps<T: Fn(i32) -> i32>(maze: &mut [i32], inrementer: T) -> u32 {
+    let mut current: u32 = 0;
+    let mut count = 0;
     loop {
-        if current > maze.len() {
-            break count;
+        if current >= maze.len() as u32 {
+            return count;
         }
 
-        let instruction = maze[current];
-        maze[current] += 1;
-        current += instruction;
+        let instruction = maze[current as usize];
+        maze[current as usize] = inrementer(instruction);
+        current = ((current as i32) + instruction) as u32;
+        count += 1;
     }
 }
 
@@ -31,7 +43,18 @@ mod tests {
     fn test1() {
         let input = vec![0,3,0,1,-3];
         let expected = 5;
-        let result = maze_exit_steps(&input);
+        let result = maze_exit_steps(&mut input.clone(), incrementer);
         assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test2() {
+        let input = vec![0,3,0,1,-3];
+        let mut input_clone = input.clone();
+        let expected = 10;
+        let result = maze_exit_steps(&mut input_clone, incrementer2);
+        assert_eq!(input, vec![0,3,0,1,-3]);
+        assert_eq!(expected, result);
+        assert_eq!(input_clone, vec![2, 3, 2, 3, -1]);
     }
 }
